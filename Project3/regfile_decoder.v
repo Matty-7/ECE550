75c768f427e2c5cbc8bf5f9941d9a52c4a30145e
Matty-7 <jh730@duke.edu>
2024-10-07 15:09:31 -0400

    revise decoder

--- Project3/regfile_decoder.v
+++ Project3/regfile_decoder.v
@@ -1,44 +1,49 @@
 module regfile_decoder(
-    output reg [31:0] out,
+    output [31:0] out,
     input [4:0] sel
 );
 
-    always @(*) begin
-        out = 32'b0;  // Initialize the output to zero
-        case (sel)
-            5'b00000: out[0] = 1'b1;
-            5'b00001: out[1] = 1'b1;
-            5'b00010: out[2] = 1'b1;
-            5'b00011: out[3] = 1'b1;
-            5'b00100: out[4] = 1'b1;
-            5'b00101: out[5] = 1'b1;
-            5'b00110: out[6] = 1'b1;
-            5'b00111: out[7] = 1'b1;
-            5'b01000: out[8] = 1'b1;
-            5'b01001: out[9] = 1'b1;
-            5'b01010: out[10] = 1'b1;
-            5'b01011: out[11] = 1'b1;
-            5'b01100: out[12] = 1'b1;
-            5'b01101: out[13] = 1'b1;
-            5'b01110: out[14] = 1'b1;
-            5'b01111: out[15] = 1'b1;
-            5'b10000: out[16] = 1'b1;
-            5'b10001: out[17] = 1'b1;
-            5'b10010: out[18] = 1'b1;
-            5'b10011: out[19] = 1'b1;
-            5'b10100: out[20] = 1'b1;
-            5'b10101: out[21] = 1'b1;
-            5'b10110: out[22] = 1'b1;
-            5'b10111: out[23] = 1'b1;
-            5'b11000: out[24] = 1'b1;
-            5'b11001: out[25] = 1'b1;
-            5'b11010: out[26] = 1'b1;
-            5'b11011: out[27] = 1'b1;
-            5'b11100: out[28] = 1'b1;
-            5'b11101: out[29] = 1'b1;
-            5'b11110: out[30] = 1'b1;
-            5'b11111: out[31] = 1'b1;
-        endcase
-    end
+    wire n0, n1, n2, n3, n4;  
+
+    
+    not (n0, sel[0]);
+    not (n1, sel[1]);
+    not (n2, sel[2]);
+    not (n3, sel[3]);
+    not (n4, sel[4]);
+
+    
+    and (out[0], n4, n3, n2, n1, n0); //00000
+    and (out[1], n4, n3, n2, n1, sel[0]); //00001
+    and (out[2], n4, n3, n2, sel[1], n0); //00010
+    and (out[3], n4, n3, n2, sel[1], sel[0]); //00011
+    and (out[4], n4, n3, sel[2], n1, n0); //00100
+    and (out[5], n4, n3, sel[2], n1, sel[0]); //00101
+    and (out[6], n4, n3, sel[2], sel[1], n0); //00110
+    and (out[7], n4, n3, sel[2], sel[1], sel[0]); //00111
+    and (out[8], n4, sel[3], n2, n1, n0); //01000
+    and (out[9], n4, sel[3], n2, n1, sel[0]); //01001
+    and (out[10], n4, sel[3], n2, sel[1], n0); //01010
+    and (out[11], n4, sel[3], n2, sel[1], sel[0]); //01011
+    and (out[12], n4, sel[3], sel[2], n1, n0); //01100
+    and (out[13], n4, sel[3], sel[2], n1, sel[0]); //01101
+    and (out[14], n4, sel[3], sel[2], sel[1], n0); //01110
+    and (out[15], n4, sel[3], sel[2], sel[1], sel[0]); //01111
+    and (out[16], sel[4], n3, n2, n1, n0); //10000
+    and (out[17], sel[4], n3, n2, n1, sel[0]); //10001
+    and (out[18], sel[4], n3, n2, sel[1], n0); //10010
+    and (out[19], sel[4], n3, n2, sel[1], sel[0]); //10011
+    and (out[20], sel[4], n3, sel[2], n1, n0); //10100
+    and (out[21], sel[4], n3, sel[2], n1, sel[0]); //10101
+    and (out[22], sel[4], n3, sel[2], sel[1], n0); //10110
+    and (out[23], sel[4], n3, sel[2], sel[1], sel[0]); //10111
+    and (out[24], sel[4], sel[3], n2, n1, n0); //11000
+    and (out[25], sel[4], sel[3], n2, n1, sel[0]); //11001
+    and (out[26], sel[4], sel[3], n2, sel[1], n0); //11010
+    and (out[27], sel[4], sel[3], n2, sel[1], sel[0]); //11011
+    and (out[28], sel[4], sel[3], sel[2], n1, n0); //11100
+    and (out[29], sel[4], sel[3], sel[2], n1, sel[0]); //11101
+    and (out[30], sel[4], sel[3], sel[2], sel[1], n0); //11110
+    and (out[31], sel[4], sel[3], sel[2], sel[1], sel[0]); //11111
 
 endmodule
